75f4ce42f7f4dec3a3cba985a3c7aafd6cd87b37
Finn Kiesinger <fkiesinger@me.com>
2019-02-08 22:05:04 +0100

    finished project

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,25 +10,56 @@ import UIKit
 
 class LeagueVC: UIViewController {
 
+    
+    var player: Player!
+    @IBOutlet weak var nxtBtn: BorderButton!
     @IBAction func onNextTapped(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
         
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        player = Player()
+        
+        
+    }
 
+    @IBOutlet weak var menBut: BorderButton!
+    @IBOutlet weak var womenBut: BorderButton!
+    @IBOutlet weak var coedBut: BorderButton!
+    @IBAction func mensTapped(_ sender: Any) {
+        initLeague(league: "m")
+    }
+    @IBAction func womensTapped(_ sender: Any) {
+        initLeague(league: "w")
+    }
+    @IBAction func coedTapped(_ sender: Any) {
+        initLeague(league: "c")
+    }
+    
+    func initLeague(league: String) {
+        player.desiredLeague = league
+        nxtBtn.isEnabled = true
+        if league == "m" {
+            menBut.layer.borderColor = UIColor.green.cgColor
+            womenBut.layer.borderColor = UIColor.white.cgColor
+            coedBut.layer.borderColor = UIColor.white.cgColor
+        } else if league == "w" {
+            womenBut.layer.borderColor = UIColor.green.cgColor
+            menBut.layer.borderColor = UIColor.white.cgColor
+            coedBut.layer.borderColor = UIColor.white.cgColor
+        } else {
+            coedBut.layer.borderColor = UIColor.green.cgColor
+            menBut.layer.borderColor = UIColor.white.cgColor
+            womenBut.layer.borderColor = UIColor.white.cgColor
+        }
         
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
     }
-    */
-
+    
 }
---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Finn Kiesinger on 08.02.19.
+//  Copyright © 2019 Finn Kiesinger. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
